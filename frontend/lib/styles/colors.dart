729991c56c7a28523c17e7b2679446da6a4a1d59
visontail <the051425@gmail.com>
2024-11-17 14:30:18 +0100

    add colors from design

---
+++ frontend/lib/styles/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color white = Color(0xFFFDFA);
+const Color paleWhite = Color(0xD4D4D4);
+const Color mainGreen = Color(0x48872B;
+const Color paleGreen = Color(0x485842);
+const Color darkGreen = Color(0x293325);
