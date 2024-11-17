e25173fe106807abf215d400a89d40a75db257e6
visontail <the051425@gmail.com>
2024-11-17 17:33:05 +0100

    implement reg, log UI screens

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         ],
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
-          home: RegistrationScreen(),
+          home: LoginScreen(),
         ),
       );
   }
