f676072fbe81fdeaa1dd8dd043ee794e85c32378
visontail <the051425@gmail.com>
2024-11-17 17:32:48 +0100

    modify color codes to work with flutter

--- frontend/lib/styles/colors.dart
+++ frontend/lib/styles/colors.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-const Color white = Color(0xFFFDFA);
-const Color gray = Color(0xD4D4D4);
-const Color black = Color(0x000000);
-const Color mainGreen = Color(0x48872B);
-const Color paleGreen = Color(0x485842);
-const Color darkGreen = Color(0x293325);
+// Define custom colors
+const Color white = Color(0xFFFDFDFD);
+const Color gray = Color(0xFFD4D4D4);
+const Color black = Colors.black;
+const Color mainGreen = Color(0xFF48872B);
+const Color paleGreen = Color(0xFF485842);
+const Color darkGreen = Color(0xFF293325);
