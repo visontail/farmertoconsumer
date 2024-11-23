0d883b05c0dcea43bccd9f566f8295b223358a5f
martinsolti <martinsolti@gmail.com>
2024-11-23 16:39:21 +0100

    implemented bar

--- frontend/lib/screens/feed/feed.dart
+++ frontend/lib/screens/feed/feed.dart
@@ -2,7 +2,9 @@ import 'package:farmertoconsumer/screens/feed/components/feed-categories.dart';
 import 'package:farmertoconsumer/screens/feed/components/feed-products.dart';
 import 'package:farmertoconsumer/screens/feed/components/feed-search.dart';
 import 'package:farmertoconsumer/screens/feed/feed_data_provider.dart';
+import 'package:farmertoconsumer/styles/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 
 class FeedScreen extends StatefulWidget {
@@ -18,13 +20,43 @@ class _FeedScreenState extends State<FeedScreen> {
     final provider = Provider.of<FeedDataProvider>(context);
 
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Products'),
-        ),
         body: RefreshIndicator(
             onRefresh: provider.refresh,
             child: Column(
               children: [
+                Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 30, 10, 10),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SizedBox(
+                              height: 50,
+                              child: Row(
+                                children: [
+                                  SvgPicture.asset(
+                                    'assets/icons/icon.svg',
+                                    width: 40,
+                                    color: mainGreen,
+                                  ),
+                                  const Padding(
+                                      padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                                      child: Text(
+                                        "Welcome",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20,
+                                        ),
+                                      ))
+                                ],
+                              )),
+                          Padding(
+                              padding: const EdgeInsets.all(5),
+                              child: SvgPicture.asset(
+                                'assets/icons/profile.svg',
+                                width: 25,
+                                color: mainGreen,
+                              ))
+                        ])),
                 FeedSearch(),
                 const Padding(
                     padding: EdgeInsets.all(15),
