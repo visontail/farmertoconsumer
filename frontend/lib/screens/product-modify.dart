feb3da4fc4e5a8f38f25277004b852773da883dc
qvc9m4 <qvc9m4@inf.elte.hu>
2024-11-27 12:08:13 +0100

    updated the product-modify page

--- frontend/lib/screens/product-modify.dart
+++ frontend/lib/screens/product-modify.dart
@@ -6,46 +6,79 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import '../styles/colors.dart';
 
-class ProductCreateForm extends StatefulWidget {
+class ProductModifyForm extends StatefulWidget {
   @override
   _ProductCreateFormState createState() => _ProductCreateFormState();
 }
 
-class _ProductCreateFormState extends State<ProductCreateForm> {
+class _ProductCreateFormState extends State<ProductModifyForm> {
   final _formKey = GlobalKey<FormState>(); // Űrlap állapot kulcsa
 
-  // TODO
-  // product create -> using product, product category, producer data/user data, quantity units
-
-
-  final TextEditingController nameController = TextEditingController();
-  final TextEditingController typeController = TextEditingController();
-  final TextEditingController categoryController = TextEditingController();
-  final TextEditingController metricController = TextEditingController();
+  String name = "golden carrot";
+  String category = "carrot";
+  String metric = "kg";
+  int price = 0;
   int quantity = 1;
-  final TextEditingController priceController = TextEditingController();
-  final TextEditingController descriptionController = TextEditingController();
+  String description = "asdasdasdasdsadasdasd";
+
+  // Kezdő értékek inicializálása a TextEditingController-ekkel
+  late final TextEditingController nameController;
+  late final TextEditingController categoryController;
+  late final TextEditingController metricController;
+  late final TextEditingController priceController;
+  late final TextEditingController descriptionController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Alapértelmezett értékek beállítása a TextEditingController-ekben
+    nameController = TextEditingController(text: name);
+    categoryController = TextEditingController(text: category);
+    metricController = TextEditingController(text: metric);
+    priceController = TextEditingController(text: price.toString());
+    descriptionController = TextEditingController(text: description);
+  }
+
+  @override
+  void dispose() {
+    // Felszabadítás: ne legyen memória szivárgás
+    nameController.dispose();
+    categoryController.dispose();
+    metricController.dispose();
+    priceController.dispose();
+    descriptionController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       appBar: AppBar(
         title: Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            SvgPicture.asset('assets/icons/icon.svg', width: 30, height: 30, color: white,)
+            SvgPicture.asset(
+              'assets/icons/icon.svg',
+              width: 30,
+              height: 30,
+              color: white,
+            ),
           ],
         ),
         leading: IconButton(
-        icon: SvgPicture.asset('assets/icons/back-arrow.svg', width: 30, height: 30, color: white,),
-        onPressed: () {
-          Navigator.pop(context); // Visszatérés az előző képernyőre
+          icon: SvgPicture.asset(
+            'assets/icons/back-arrow.svg',
+            width: 30,
+            height: 30,
+            color: white,
+          ),
+          onPressed: () {
+            Navigator.pop(context); // Visszatérés az előző képernyőre
           },
         ),
-      backgroundColor: mainGreen,
+        backgroundColor: mainGreen,
       ),
-
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
         child: Form(
@@ -53,185 +86,163 @@ class _ProductCreateFormState extends State<ProductCreateForm> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              
-              // Drag-and-drop fájlfeltöltő
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsets.all(16.0),
-              decoration: BoxDecoration(
-                color: Colors.grey[200],
-                borderRadius: BorderRadius.circular(8.0),
-                border: Border.all(color: Colors.grey),
+              //Product name
+              Text('Product Name'),
+              TextFormField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  hintText: 'product Name',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a product name';
+                  }
+                  return null;
+                },
               ),
-              child: Column(
-                children: const [
-                  Icon(Icons.upload_file, size: 40, color: Colors.grey),
-                  SizedBox(height: 8),
-                  Text(
-                    "Drag drop some files here, or click to select files",
-                    style: TextStyle(color: Colors.grey),
-                    textAlign: TextAlign.center,
+              const SizedBox(height: 16),
+
+              //Category
+              Text('Category'),
+              TextFormField(
+                controller: categoryController,
+                decoration: const InputDecoration(
+                  hintText: 'category',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a product category';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+
+              //Metric
+              Text('Metric'),
+              TextFormField(
+                controller: metricController,
+                decoration: const InputDecoration(
+                  hintText: 'metric',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a metric';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+
+              //Price
+              Text('Price'),
+              TextFormField(
+                controller: priceController,
+                decoration: const InputDecoration(
+                  hintText: 'price',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a price';
+                  }
+                  if (double.tryParse(value) == null) {
+                    return 'Please enter a valid number';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+
+              //Quantity            
+              Row(
+                children: [
+                  const Text("Quantity"),
+                  IconButton(
+                    icon: const Icon(Icons.remove_circle, color: mainGreen),
+                    onPressed: () {
+                      setState((){
+                        if(quantity > 1){
+                          quantity--;
+                        }
+                      });
+                    },
                   ),
-                  SizedBox(height: 4),
-                  Text(
-                    "* 1080 x 1080 (1:1) recommended, up to 2MB each",
-                    style: TextStyle(color: Colors.grey, fontSize: 12),
+                  Text(quantity.toString()), 
+                  IconButton(
+                    icon: const Icon(Icons.add_circle, color: mainGreen),
+                    onPressed: () {
+                      setState((){
+                        quantity++;
+                      });
+                    },
                   ),
                 ],
               ),
-            ),
-            const SizedBox(height: 16),
-            
+              const SizedBox(height: 16),
 
-            
-            //name
-            Text('Product Name'),
-            TextFormField(
-              controller: nameController,
-              decoration: const InputDecoration(
-                hintText: 'product Name',
-                border: OutlineInputBorder(),
+              //Description
+              Text('Description'),
+              TextFormField(
+                controller: descriptionController,
+                maxLines: 4,
+                decoration: const InputDecoration(
+                  hintText: 'description',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a description';
+                  }
+                  return null;
+                },
               ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter a product name';
-                }
-                 return null;
-              },
-            ),
-            const SizedBox(height: 16),
-            
-            //category
-            Text('Category'),
-            TextFormField(
-              controller: categoryController,
-              decoration: const InputDecoration(
-                hintText: 'category',
-                border: OutlineInputBorder(),
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter a product category';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 16),
-            
-            //metric
-            Text('Metric'),
-            TextFormField(
-              controller: metricController,
-              decoration: const InputDecoration(
-                hintText: 'metric',
-                border: OutlineInputBorder(),
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter a metric';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 16),
+              const SizedBox(height: 16),
 
-            //quantity            
-            Row(
-              children: [
-                const Text("quantity"),
-                IconButton(
-                  icon: const Icon(Icons.remove_circle, color: mainGreen),
-                  onPressed: () {
-                    setState((){
-                      if(quantity > 1){
-                        quantity--;
+              // Save és Delete gombok (változatlanok maradnak)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        print('Product name: ${nameController.text}');
+                        print('Product category: ${categoryController.text}');
+                        print('Product metric: ${metricController.text}');
+                        print('Price: ${priceController.text}');
+                        print('Product quantity: ${quantity}');
+                        print('Product description: ${descriptionController.text}');
+                        
+                        name = nameController.text;
+                        category = categoryController.text;
+                        metric = metricController.text;
+                        description = descriptionController.text;
+
                       }
-                    });
-                  },
-                ),
-                Text(quantity.toString()), 
-                IconButton(
-                  icon: const Icon(Icons.add_circle, color: mainGreen),
-                  onPressed: () {
-                    setState((){
-                      quantity++;
-                    });
-                  },
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-
-            //price
-            Text('Price' ),
-            TextFormField(
-              controller: priceController,
-              decoration: const InputDecoration(
-                hintText: 'price',
-                border: OutlineInputBorder(),
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: mainGreen, foregroundColor: white),
+                    child: const Text('Save Product'),
+                  ),
+                  const SizedBox(width: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      // delete product logic
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red, foregroundColor: white),
+                    child: const Text('Delete Product'),
+                  ),
+                ],
               ),
-              keyboardType: TextInputType.number,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter a price';
-                }
-                if (double.tryParse(value) == null) {
-                  return 'Please enter a valid number';
-                 }
-                return null;
-              },
-            ),
-            const SizedBox(height: 16),
-
-            //description
-            Text('Description'),
-            TextFormField(
-              controller: descriptionController,
-              maxLines: 4,
-              decoration: const InputDecoration(
-                hintText: 'descrtiption',
-                border: OutlineInputBorder(),
-                
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter a description';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 16),
-            
-            // Mentés gomb
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [ElevatedButton(
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  // Az adatok validak, itt dolgozhatod fel őket
-                  print('Product name: ${nameController.text}');
-                  print('Product type: ${typeController.text}');
-                  print('Product category: ${categoryController.text}');
-                  print('Product metric: ${metricController.text}');
-                  print('Product quantity: ${quantity}');
-                  print('Product description: ${descriptionController.text}');
-                  print('Price: ${priceController.text}');
-
-                  
-                  //Navigator.pop(context); //navigate back to producer profile
-                }
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: mainGreen,
-                foregroundColor: white),
-              child: const Text('Add Product'),
-            ),],
-            ),
-            
-          ],
+            ],
+          ),
         ),
       ),
-    ),
-  );
+    );
   }
 }
