81d8704adce8f92733908e8ce93a5d55bdde9937
qvc9m4 <qvc9m4@inf.elte.hu>
2024-11-29 12:29:18 +0100

    created quantityUnit_service

---
+++ frontend/lib/services/quantityUnit_service.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+import 'package:farmertoconsumer/models/quantityUnit.dart';
+import 'package:farmertoconsumer/services/common/get_all_return_value.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../utils/api_endpoints.dart';
+
+class QuantityUnitService extends ChangeNotifier {
+  static const Map<String, String> headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  };
+
+  Future<GetAllReturnValue<QuantityUnit>> getAll() async {
+    final response = await http.get(
+      Uri.parse(getAllCategoryEndpoint),
+      headers: headers,
+    );
+
+    final responseBody = json.decode(response.body);
+
+    return GetAllReturnValue(
+        total: responseBody["total"],
+        current: responseBody["current"],
+        data: (responseBody["quantityUnits"] as List<dynamic>)
+        .map((e) => QuantityUnit.fromJson(e))
+        .toList());
+  }
+}
