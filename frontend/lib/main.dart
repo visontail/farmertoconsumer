093b0e4b0da9fd00fd562e59ed289072eaca2432
RolandSandorNagy <roland.s.nagy@gmail.com>
2024-12-05 18:22:56 +0100

    Merge branch 'main' into feature/consumer_profile

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,25 +1,22 @@
-import 'package:farmertoconsumer/screens/product.dart';
-import 'package:farmertoconsumer/services/product_service.dart';
+import 'package:farmertoconsumer/providers/auth_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:farmertoconsumer/services/product_service.dart';
 
 import '../screens/feed/feed.dart';
+import '../screens/profile.dart';
 //import '../screens/consumer_profile.dart';
 import '../screens/login.dart';
-//import '../screens/order_confirmation.dart';
+import '../screens/order_confirmation.dart';
 import '../screens/order.dart';
 //import '../screens/producer_profile.dart';
-//import '../screens/product_management.dart';
-//import '../screens/product_upload.dart';
+import '../screens/product_management.dart';
+import '../screens/product_upload.dart';
 import '../screens/product.dart';
 import '../screens/registration.dart';
-//import '../screens/user_upgrade_form.dart';
+import '../screens/user_upgrade_form.dart';
 import '../screens/user_upgrade.dart';
 
-import '../screens/profile.dart';
-
-import '../services/auth_service.dart';
-import '../services/product_service.dart';
 import '../screens/feed/feed_data_provider.dart';
 import '../utils/routes.dart';
 
@@ -36,25 +33,25 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (context) => AuthProvider()),
         ChangeNotifierProvider(create: (context) => FeedDataProvider()),
-        ChangeNotifierProvider(create: (context) => ProductService()),
+        //ChangeNotifierProvider(create: (context) => ProductService()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        initialRoute: '/profile',
+        initialRoute: '/',
         routes: {
-          //'/': (context) => const FeedScreen(),
+          '/': (context) => const FeedScreen(),
           Routes.feed: (context) => FeedScreen(),
           Routes.login: (context) => LoginScreen(),
           Routes.registration: (context) => RegistrationScreen(),
-//          Routes.order: (context) => OrderScreen(),
-          //Routes.orderConfirmation: (context) => OrderConfirmationScreen(),
+          Routes.order: (context) => OrderScreen(orderId: '1'),
+          Routes.orderConfirmation: (context) => OrderConfirmationScreen(),
           Routes.profile: (context) => ProfileScreen(),
           //Routes.consumerProfile: (context) => ConsumerProfileScreen(),
           //Routes.producerProfile: (context) => ProducerProfileScreen(),
-          //Routes.productManagement: (context) => ProductManagementScreen(),
-          //Routes.productUpload: (context) => ProductUploadScreen(),
+          Routes.productManagement: (context) => ProductManagementScreen(),
+          Routes.productUpload: (context) => ProductUploadScreen(),
           Routes.userUpgrade: (context) => UserUpgradeScreen(),
-          //Routes.userUpgradeForm: (context) => UserUpgradeFormScreen(),
+          Routes.userUpgradeForm: (context) => UserUpgradeFormScreen(),
         },
         onGenerateRoute: (settings) {
           if (settings.name == Routes.product) {
